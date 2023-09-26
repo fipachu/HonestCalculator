a7d68059282772ff9119ce98e88b91db22cc1b1e
fipachu <80906036+fipachu@users.noreply.github.com>
2023-09-26 08:58:13 +0200

    stage 2, rewritten in a way I deem satisfying

--- Honest Calculator/task/honest_calc.py
+++ Honest Calculator/task/honest_calc.py
@@ -1,19 +1,96 @@
-# Stage 1, procedural edition.
-# It seems like it works best as procedural code.
-while True:
-    equation = input('Enter an equation\n')
-    x, oper, y = equation.split()
+ALLOWED_OPERATORS = '+-*/'
 
+
+def is_allowed(s: str) -> bool:
+    if s in ALLOWED_OPERATORS:
+        return True
+    else:
+        return False
+
+
+def is_number(s: str) -> bool:
     try:
-        float(x)
-        float(y)
+        float(s)
     except ValueError:
-        print('Do you even know what numbers are? Stay focused!')
-        continue
+        return False
+    else:
+        return True
 
-    if oper not in '+-*/':
-        print("Yes ... an interesting math operation. "
-              "You've slept through all classes, haven't you?")
-        continue
 
-    break
+def calculate(x: float, y: float, oper: str) -> float:
+    match oper:
+        case '+':
+            return x+y
+        case '-':
+            return x-y
+        case '*':
+            return x*y
+        case '/':
+            return x/y
+        case _:
+            raise ValueError(f"Wrong operator! '{oper}' "
+                             f"not in '{ALLOWED_OPERATORS}'")
+    # # Alternatively, against the spirit of the exercise:
+    # for op in ALLOWED_OPERATORS:
+    #     if op == oper:
+    #         return eval(f'{x}{op}{y}')
+    # else:
+    #     raise ValueError(f"Wrong operator! '{oper}' "
+    #                      f"not in '{ALLOWED_OPERATORS}'")
+    # # I wonder if there is a way to allow for using
+    # # user defined lists of operators without using eval()
+
+
+def ui_loop() -> None:
+    while True:
+        equation = input('Enter an equation\n')
+        x, oper, y = equation.split()
+
+        if is_number(x) and is_number(y):
+            x, y = float(x), float(y)
+        else:
+            print('Do you even know what numbers are? Stay focused!')
+            continue
+
+        if not is_allowed(oper):
+            print("Yes ... an interesting math operation. "
+                  "You've slept through all classes, haven't you?")
+            continue
+
+        try:
+            result = calculate(x, y, oper)
+        except ZeroDivisionError:
+            print('Yeah... division by zero. Smart move...')
+            continue
+
+        print(result)
+        return
+
+
+def main():
+    ui_loop()
+
+
+if __name__ == "__main__":
+    main()
+
+
+# Nothing interesting here, move on.
+def evil_hack():
+    while True:
+        equation = input('Enter an equation\n')
+        try:
+            print(eval(equation))
+            return
+        except NameError:
+            print('Do you even know what numbers are? Stay focused!')
+            continue
+        except SyntaxError:
+            print("Yes ... an interesting math operation. "
+                  "You've slept through all classes, haven't you?")
+            continue
+        except ZeroDivisionError:
+            print('Yeah... division by zero. Smart move...')
+            continue
+    # Seriously though, they thought of it. The unit test knows
+    # if you used eval() in your solution.
