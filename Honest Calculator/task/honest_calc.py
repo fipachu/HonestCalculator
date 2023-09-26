265a843b5cdc5340bf9710f128a337e707afed31
fipachu <80906036+fipachu@users.noreply.github.com>
2023-09-26 07:20:36 +0200

    rewritten stage 1 AGAIN

--- Honest Calculator/task/honest_calc.py
+++ Honest Calculator/task/honest_calc.py
@@ -1,46 +1,19 @@
-OK = 'ok'
-BAD_NUMBER = 'bad_number'
-BAD_OPER = 'bad_oper'
-OPERATORS = '+-*/'
-
-
-def process_input() -> str:
-    calc = input()
-    x, oper, y = calc.split()
+# Stage 1, procedural edition.
+# It seems like it works best as procedural code.
+while True:
+    equation = input('Enter an equation\n')
+    x, oper, y = equation.split()
 
     try:
         float(x)
         float(y)
     except ValueError:
-        return BAD_NUMBER
+        print('Do you even know what numbers are? Stay focused!')
+        continue
 
-    if oper not in OPERATORS:
-        # split in two lines to comply with PEP 8
-        return BAD_OPER
+    if oper not in '+-*/':
+        print("Yes ... an interesting math operation. "
+              "You've slept through all classes, haven't you?")
+        continue
 
-    return OK
-
-
-def ui_loop() -> None:
-    while True:
-        print('Enter an equation')
-        control = process_input()
-        if control == BAD_NUMBER:
-            print('Do you even know what numbers are? Stay focused!')
-            continue
-        elif control == BAD_OPER:
-            print("Yes ... an interesting math operation. "
-                  "You've slept through all classes, haven't you?")
-            continue
-        elif control == OK:
-            return
-        else:
-            raise ValueError(f"Invalid control '{control}'")
-
-
-def main():
-    ui_loop()
-
-
-if __name__ == "__main__":
-    main()
+    break
