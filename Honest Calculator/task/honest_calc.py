97e2d64215ddcbf5c395e5642461bf235e25cde3
fipachu <80906036+fipachu@users.noreply.github.com>
2023-09-27 13:34:45 +0200

    stage 3 done

--- Honest Calculator/task/honest_calc.py
+++ Honest Calculator/task/honest_calc.py
@@ -1,4 +1,6 @@
 ALLOWED_OPERATORS = '+-*/'
+M = 'M'
+Y, N = 'y', 'n'
 
 
 def is_allowed(s: str) -> bool:
@@ -41,10 +43,38 @@ def calculate(x: float, y: float, oper: str) -> float:
     # # user defined lists of operators without using eval()
 
 
-def ui_loop() -> None:
+def keep_going() -> bool:
+    while True:
+        answer = input('Do you want to continue calculations? (y / n):\n')
+        if answer == Y:
+            return True
+        elif answer == N:
+            return False
+        else:
+            continue
+
+
+def remember() -> bool:
+    while True:
+        answer = input('Do you want to store the result? (y / n):\n')
+        if answer == Y:
+            return True
+        elif answer == N:
+            return False
+        else:
+            continue
+
+
+def calculator() -> None:
+    memory: float = 0  # Apparently it has to be a float.
     while True:
         equation = input('Enter an equation\n')
         x, oper, y = equation.split()
+
+        if x == M:
+            x = memory
+        if y == M:
+            y = memory
 
         if is_number(x) and is_number(y):
             x, y = float(x), float(y)
@@ -62,35 +92,20 @@ def ui_loop() -> None:
         except ZeroDivisionError:
             print('Yeah... division by zero. Smart move...')
             continue
-
         print(result)
-        return
+
+        if remember():
+            memory = result
+
+        if keep_going():
+            continue
+        else:
+            return
 
 
 def main():
-    ui_loop()
+    calculator()
 
 
 if __name__ == "__main__":
     main()
-
-
-# Nothing interesting here, move on.
-def evil_hack():
-    while True:
-        equation = input('Enter an equation\n')
-        try:
-            print(eval(equation))
-            return
-        except NameError:
-            print('Do you even know what numbers are? Stay focused!')
-            continue
-        except SyntaxError:
-            print("Yes ... an interesting math operation. "
-                  "You've slept through all classes, haven't you?")
-            continue
-        except ZeroDivisionError:
-            print('Yeah... division by zero. Smart move...')
-            continue
-    # Seriously though, they thought of it. The unit test knows
-    # if you used eval() in your solution.
